1a8ae40dca212a8f2f47bc8971f7439b233f9c13
Kayxzy <106707683+Kayxzy@users.noreply.github.com>
2022-09-08 01:45:54 +0700

    Update helper_func.py

--- helper_func.py
+++ helper_func.py
@@ -23,10 +23,6 @@ async def is_subscribed(filter, client, update):
         return True
     if not FORCE_SUB_GROUP:
         return True
-    if not FORCE_SUB_GROUP1:
-        return True
-    if not FORCE_SUB_GROUP2:
-        return True
     user_id = update.from_user.id
     if user_id in ADMINS:
         return True
@@ -34,9 +30,7 @@ async def is_subscribed(filter, client, update):
         member = await client.get_chat_member(chat_id=FORCE_SUB_CHANNEL, user_id=user_id)
         member = await client.get_chat_member(chat_id=FORCE_SUB_GROUP, user_id=user_id)
         member = await client.get_chat_member(chat_id=FORCE_SUB_CHANNEL1, user_id=user_id)
-        member = await client.get_chat_member(chat_id=FORCE_SUB_GROUP1, user_id=user_id)
         member = await client.get_chat_member(chat_id=FORCE_SUB_CHANNEL2, user_id=user_id)
-        member = await client.get_chat_member(chat_id=FORCE_SUB_GROUP2, user_id=user_id)
     except UserNotParticipant:
         return False
 
